6aff372e2d411fd2332dfdc454edec2c987518c7
ren1989lisa <renata.ortizchavez19@utez.edu.mx>
2025-12-09 11:09:39 -0600

    Correcciones de retrofit

--- app/src/main/java/com/example/saltasalta/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/saltasalta/data/api/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://192.168.0.8:5000/"
+    private const val BASE_URL = "http://192.168.107.139:5000/"
     
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
